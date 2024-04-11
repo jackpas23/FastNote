42ef43761a51453c3ecbc2aca3e55337dcf1e932
Flaxo P <jack.f.pas@gmail.com>
2024-04-11 16:17:48 -0400

    i think i got it

--- set-up.py
+++ set-up.py
@@ -12,9 +12,9 @@ def prep():
     print("If you prefer an easy installation, simply press ENTER, and a default path will be used.")
     print("Example of a full path: /home/username/Documents/notes.txt")
     print("\nPlease enter the path now (or press ENTER for easy installation):", end=' ')
-    flavor=input("You Kali(a) or Ubuntu(b) bruh?:").strip()
+    flavor=input("You Kali(1) or Ubuntu(2) bruh?:").strip()
     print(flavor)
-    if flavor != 'a' or 'b':
+    if flavor != "1" or "2":
         flavor=input("You Kali(1) or Ubuntu(2) dumbass:").strip()
 
     try:
